17d9abdafea61d1303af77fe5f880a2072c30dd7
Jair Anguiano <jairanpo@gmail.com>
2020-08-17 15:39:29 -0500

    sequencerRepair

---
+++ sequencerRepair.py
@@ -0,0 +1,21 @@
+import pymel.core as pm
+
+try:
+    pm.delete("sequencer1")
+except:
+    pass
+
+sequencer = pm.createNode("sequencer")
+manager = pm.ls(type="sequenceManager")[0]
+
+list_of_shots = pm.ls(type="shot")
+
+for index, shot in enumerate(list_of_shots):
+    shot.message.disconnect()
+    shot.message.connect(sequencer.shots[index])
+    try:
+        manager.sequences[index].disconnect()
+    except:
+        pass
+
+sequencer.message.connect(manager.sequences[0])
